bd6291b17f830f7c6de4f365d738db045d4a524d
carolineausema <52717803+carolineeausema@users.noreply.github.com>
2024-11-15 18:14:39 -0800

    pls work

--- components/P5Wrapper.jsx
+++ components/P5Wrapper.jsx
@@ -1,17 +1,26 @@
-// components/p5Wrapper.js
+// components/P5Wrapper.js
 import React, { useEffect, useRef } from 'react';
 import p5 from 'p5';
 
 const P5Wrapper = ({ sketch }) => {
   const wrapperRef = useRef();
+  const p5InstanceRef = useRef(null);
 
   useEffect(() => {
-    const canvasParentRef = wrapperRef.current;
+    // Ensure p5 instance is only created once per mount
+    if (wrapperRef.current) {
+      p5InstanceRef.current = new p5(sketch, wrapperRef.current);
+    }
 
-    new p5(sketch, canvasParentRef);
+    // Cleanup the p5 instance on component unmount
+    return () => {
+      if (p5InstanceRef.current) {
+        p5InstanceRef.current.remove();
+      }
+    };
   }, [sketch]);
 
-  return <div ref={wrapperRef}></div>;
+  return <div ref={wrapperRef} style={{ position: 'relative' }}></div>;
 };
 
 export default P5Wrapper;
